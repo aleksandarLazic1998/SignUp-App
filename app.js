b53ffc33c193866238e2ec792b398739c01df9f6
aleksandarLazic191277 <75834819+aleksandarLazic191277@users.noreply.github.com>
2021-03-22 20:08:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+//jshint esversion: 6
+'use strict';
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Getting home page request
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+});
+
+//posting on home page
+app.post('/', (req, res) => { 
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+
+            }
+        }
+    ]
+};
+
+app.post('/failure',(req,res)=>{
+    res.redirect('/');
+})
+
+    const jsonData = JSON.stringify(data);
+
+    // https.request(url, options,(response){})
+    const url = `https://us1.api.mailchimp.com/3.0/lists/004d9d609a`;
+
+    const options = {
+        method: 'POST',
+        auth: 'acafaca1:13f590ddfc598451209646d13aca13cb-us1'
+    };
+
+    const request = https.request(url, options, (response) => {
+if(response.statusCode=== 200){
+    
+        res.sendFile(__dirname + '/success.html');
+    
+}else{
+    
+        res.sendFile(__dirname + '/failure.html');
+
+    
+}
+
+        response.on('data', (data) => {
+            console.log(JSON.parse(data));
+        })
+    });
+    request.write(jsonData);
+    request.end();
+
+});
+
+
+//Listening for server
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is working on port ${process.env.PORT} !`);
+});
+
+
+//ApiKey
+//13f590ddfc598451209646d13aca13cb-us1
+
+//listId
+//004d9d609a
